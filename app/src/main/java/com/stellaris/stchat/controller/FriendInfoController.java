f52eb0c0df60894d968735211646d20045c9f6d8
Jamesfico <627342306@qq.com>
2019-11-21 22:02:48 +0800

    添加了四个文件，实现了点开头像的用户信息界面和“发消息”的按钮 亟待解决的问题：get不到数据库上用户的信息 删除了部分代码，部分功能不全（似乎是头像相关的部分？

---
+++ app/src/main/java/com/stellaris/stchat/controller/FriendInfoController.java
@@ -0,0 +1,51 @@
+package com.stellaris.stchat.controller;
+
+import android.content.Intent;
+import android.view.View;
+
+import cn.jpush.im.android.api.model.UserInfo;
+import com.stellaris.stchat.R;
+import com.stellaris.stchat.activity.FriendInfoActivity;
+
+/**
+ * Created by ${chenyn} on 2017/3/24.
+ */
+
+public class FriendInfoController implements View.OnClickListener {
+    private FriendInfoActivity mContext;
+    private UserInfo friendInfo;
+    private int flags;
+
+    public FriendInfoController(int flags, FriendInfoActivity context) {
+        this.mContext = context;
+        this.flags = flags;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_goToChat:
+                mContext.startChatActivity();
+                break;
+            case R.id.iv_friendPhoto:
+                mContext.startBrowserAvatar();
+                break;
+            case R.id.jmui_commit_btn:
+                Intent intent = new Intent();
+                intent.putExtra("userName", friendInfo.getUserName());
+                intent.putExtra("noteName", friendInfo.getNotename());
+                mContext.startActivity(intent);
+                break;
+            case R.id.return_btn:
+                mContext.finish();
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void setFriendInfo(UserInfo info) {
+        friendInfo = info;
+    }
+
+}
